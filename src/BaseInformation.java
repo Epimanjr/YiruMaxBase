035043e275824587202079b07540e689e001a359
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-08 17:08:28 +0100

    Introduction d'une HashMap.
    Elle est utilisée pour la gestion des informations, pour la connexion à
    la base de données.

--- src/BaseInformation.java
+++ src/BaseInformation.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  *
@@ -15,131 +16,44 @@ import java.io.Serializable;
 public final class BaseInformation implements Serializable {
 
     /**
-     * For instance oracle or mysql.
+     * Map qui gère toutes les données
      */
-    private String driver = "";
+    private HashMap<String, String> map = new HashMap<String, String>();
 
     /**
-     * In general com.mysql.jdbc.Driver.
+     * Constructeur qui initialise d'office la map.
      */
-    private String namedriver = "";
-
-    /**
-     * Database's name.
-     */
-    private String dbname = "";
-
-    /**
-     * Login.
-     */
-    private String login = "";
-
-    /**
-     * Password.
-     */
-    private String password = "";
-
-    /**
-     * URL, such as localhost.
-     */
-    private String url = "";
-
-    /**
-     * Constructor, create object and write information.
-     *
-     * @param driver driver
-     * @param namedriver namedriver
-     * @param dbname dbname
-     * @param login login
-     * @param password password
-     * @param url url
-     */
-    public BaseInformation(String driver, String namedriver, String dbname, String login,
-            String password, String url) {
-        super();
-
-        this.driver = driver;
-        this.namedriver = namedriver;
-        this.dbname = dbname;
-        this.login = login;
-        this.password = password;
-        this.url = url;
-
-        // Write information.
-        this.ecrireInformations();
+    public BaseInformation(HashMap<String, String> maplu) {
+    	this.map = maplu;
     }
 
     /**
-     * Returns driver name, normally needn't.
-     *
-     * @return Driver name
+     * Constructeur vide.
+     * Se contente de remplir avec les valeurs par défaut.
      */
-    public String getNamedriver() {
-        return namedriver;
+    public BaseInformation() {
+    	map.put("driver", "mysql");
+    	map.put("namedriver", "com.mysql.jdbc.Driver");
+    	map.put("dbname", "dbname");
+    	map.put("login", "root");
+    	map.put("password", "");
+    	map.put("url", "//localhost");
     }
 
-    /**
-     * Returns database URL, essential to have a connection.
-     *
-     * @return database url
-     */
-    public String getUrl() {
-        return url;
-    }
-
-    /**
-     * Returns the driver, eighter oracle, or mysql most often.
-     *
-     * @return driver
-     */
-    public String getDriver() {
-        return driver;
-    }
-
-    /**
-     * Returns the name of table, needed to get connection.
-     *
-     * @return table name
-     */
-    public String getDbname() {
-        return dbname;
-    }
-
-    /**
-     * Returns user's login.
-     *
-     * @return user login
-     */
-    public String getLogin() {
-        return login;
-    }
-
-    /**
-     * Returns user's password.
-     *
-     * @return mot de passe
-     */
-    public String getPassword() {
-        return password;
-    }
-
+   
     /**
      * Method which read all informations from infobdd.idb file.
      *
      * @return les informations utiles
      */
-    public static BaseInformation lectureInformations() {
+    public static BaseInformation lectureInformations(String filename) throws FileNotFoundException, IOException, ClassNotFoundException {
         BaseInformation res = null;
-        try {
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("donnees/infobdd.idb"))) {
-                //On récupère simplement l'objet.
-                res = (BaseInformation) ois.readObject();
-            }
-        } catch (FileNotFoundException e) {
 
-        } catch (IOException | ClassNotFoundException e) {
-
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename)) {
+            //On récupère simplement l'objet.
+            res = new BaseInformation((HashMap<String, String>) ois.readObject());
         }
+        
 
         return res;
     }
@@ -150,16 +64,11 @@ public final class BaseInformation implements Serializable {
      *
      * @return true if well-written
      */
-    public boolean ecrireInformations() {
-        try {
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("donnees/infobdd.idb"))) {
-                oos.writeObject(this);
-                return true;
-            }
-        } catch (FileNotFoundException e) {
+    public boolean ecrireInformations(String filename) throws FileNotFoundException, IOException {
 
-        } catch (IOException e) {
-
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(this);
+            return true;
         }
 
         return false;
